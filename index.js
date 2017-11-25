09fe34f4e75d0959ac3344dade7cfdac2f993dac
Noah Gao <noahgaocn@outlook.com>
2017-11-25 17:44:47 +0800

    Update return

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const config = {
 app
   .use(wechat(config).middleware(async (message, ctx) => {
     console.log(message)
+    if (message.MsgType === 'image') {
+      return 'OK!'
+    } else if (message.MsgType === 'text') {
+      if (message.Content === 'MNIST') return 'MNIST服务状态：OK'
+      else return message.Content
+    } else {
+      return ''
+    }
   }))
   .listen(2334)
